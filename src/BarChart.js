9766a2cf8840abd6db8369e85b041f84fe716031
Rickard Sunden <rickard.sunden@outlook.com>
2019-10-20 22:55:33 -0400

    Add styled components

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,45 +1,67 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 
 import Runs from './runs'
 import Run from './Run'
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  padding: 5rem;
+`
+
+function Date({ day, date }) {
+  return (
+    <div>
+      <p
+        css={`
+          font-weight: 700;
+          font-size: 0.8em;
+        `}
+      >
+        DAY {day}
+      </p>
+      <p
+        css={`
+          font-size: 0.7em;
+        `}
+      >
+        {date}
+      </p>
+    </div>
+  )
+}
+
+const ChartArea = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  flex: 1;
+  align-items: flex-end;
+  height: 200px;
+  width: 800px;
+  border-top: 1px dashed #fff;
+`
+
+const DistanceLabel = styled.div`
+  top: -7px;
+  left: -70px;
+  position: absolute;
+  font-weight: 700;
+  font-size: 0.7em;
+`
+
+const Padder = styled.div`
+  padding: 0 30px;
+`
+
 function BarChart() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'flex-end',
-        padding: '5rem'
-      }}
-    >
-      <div>
-        <p style={{ fontWeight: 700, fontSize: '0.8em' }}>DAY 1</p>
-        <p style={{ fontSize: '0.7em' }}>2019-10-18</p>
-      </div>
-      <div style={{ padding: '0 30px' }}>
-        <div
-          style={{
-            boxSizing: 'border-box',
-            display: 'flex',
-            flex: 1,
-            alignItems: 'flex-end',
-            height: '200px',
-            width: '800px',
-            borderTop: '1px dashed #fff',
-            position: 'relative'
-          }}
-        >
-          <div
-            style={{
-              top: '-7px',
-              left: '-70px',
-              position: 'absolute',
-              fontWeight: 700,
-              fontSize: '0.7em'
-            }}
-          >
-            26.2 MILES
-          </div>
+    <Wrapper>
+      <Date day={1} date={'2019-10-18'} />
+      <Padder>
+        <ChartArea>
+          <DistanceLabel>26.2 MILES</DistanceLabel>
           {Runs.items.map(run => (
             <Run
               key={run.date}
@@ -48,13 +70,10 @@ function BarChart() {
               count={Runs.count}
             />
           ))}
-        </div>
-      </div>
-      <div>
-        <p style={{ fontWeight: 700, fontSize: '0.8em' }}>DAY 136</p>
-        <p style={{ fontSize: '0.7em' }}>2020-03-01</p>
-      </div>
-    </div>
+        </ChartArea>
+      </Padder>
+      <Date day={Runs.count} date={'2020-03-01'} />
+    </Wrapper>
   )
 }
 
